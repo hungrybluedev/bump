00323875477979c280b9065e8cdf4c83a707180e
HungryBlueDev <hungrybluedev@gmail.com>
2020-10-18 20:04:52 +0530

    remove unused parameters

--- test/src/run-tests.c
+++ test/src/run-tests.c
@@ -121,8 +121,7 @@ MunitResult process_line_1_1_51() {
   return MUNIT_OK;
 }
 
-MunitResult process_two(const MunitParameter params[],
-                        void *user_data_or_fixture) {
+MunitResult process_two() {
   char line[50] = {0};
   char buffer[50] = {0};
   const char *input_line = "First we have 1.6.84, then we have 8.16.3!";
